e3d61fd8b3d99999c293752c16b8c37a7d8157f7
Leonard Seibold <leo@zrtx.de>
2025-01-03 00:56:32 +0100

    update naming, improve example

--- examples/simple-split/src/lib.rs
+++ examples/simple-split/src/lib.rs
@@ -1,12 +1,10 @@
 use leptos::prelude::*;
 use leptos_meta::*;
-use leptos_resize::{LayoutDirection, ResizableGrid};
+use leptos_resize::ResizableSplit;
 
 #[component]
 pub fn App() -> impl IntoView {
   provide_meta_context();
-
-  let col = RwSignal::new(60.);
 
   view! {
       <Html attr:lang="en" attr:dir="ltr" attr:data-theme="light" />
@@ -14,9 +12,12 @@ pub fn App() -> impl IntoView {
       <Meta charset="UTF-8" />
       <Meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-      <ResizableGrid col direction=LayoutDirection::Row grid_class="container" handle_class="handle">
-        <div class="first" />
+      <ResizableSplit grid_class="container" handle_class="handle">
+        <div class="first">
+          <h2>"Lorem ipsum dolor sit amet"</h2>
+          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."</p>
+        </div>
         <div class="second" />
-      </ResizableGrid>
+      </ResizableSplit>
   }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,45 @@
 use leptos::{ev, html::Div, prelude::*, text_prop::TextProp};
 
+/// Enum representing the direction of the split.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
-pub enum LayoutDirection {
+pub enum SplitDirection {
+  /// Split the container horizontally.
   #[default]
   Row,
+  /// Split the container vertically.
   Column,
 }
 
+/// A resizable split component. Children will be put into a grid with two
+/// columns or rows, depending on the direction.
+///
+/// # Example:
+/// ```
+/// use leptos_resize::ResizableSplit;
+///
+/// fn MyComponent() -> impl IntoView {
+///   view! {
+///     <ResizableSplit>
+///       <div>"First"</div>
+///       <div>"Second"</div>
+///     </ResizableSplit>
+///   }
+/// }
+/// ```
 #[component]
-pub fn ResizableGrid(
-  #[prop(into, optional)] direction: LayoutDirection,
-  #[prop(into, optional)] grid_class: TextProp,
-  #[prop(into, optional)] handle_class: TextProp,
-  #[prop(optional, default = RwSignal::new(50f64))] col: RwSignal<f64>,
+pub fn ResizableSplit(
+  /// The direction of the split.
+  #[prop(into, optional)]
+  direction: SplitDirection,
+  /// The class property for the underlying grid.
+  #[prop(into, optional)]
+  grid_class: TextProp,
+  /// The class property for the resize handle.
+  #[prop(into, optional)]
+  handle_class: TextProp,
+  /// The percentage of the first split half. Will update on resize.
+  #[prop(optional, default = RwSignal::new(50f64))]
+  col: RwSignal<f64>,
   children: Children,
 ) -> impl IntoView {
   let container = NodeRef::<Div>::new();
@@ -34,14 +61,58 @@ pub fn ResizableGrid(
       let bounds = container.get_bounding_client_rect();
       let x = ev.client_x() as f64 - bounds.left();
       let x_percentage = (x / bounds.width()) * 100.0;
-
       col.set(x_percentage);
+
+      col.set(match direction {
+        SplitDirection::Row => {
+          let x = ev.client_x() as f64 - bounds.left();
+          (x / bounds.width()) * 100.0
+        }
+        SplitDirection::Column => {
+          let y = ev.client_y() as f64 - bounds.top();
+          (y / bounds.height()) * 100.0
+        }
+      });
     }
   };
 
   let stop_resize = move |ev: ev::MouseEvent| {
     ev.prevent_default();
     set_resizing.set(false);
+  };
+
+  let grid_template_columns = move || match direction {
+    SplitDirection::Row => {
+      format!("{:.2}% {:.2}%", col.get(), 100.0 - col.get())
+    }
+    SplitDirection::Column => "100%".into(),
+  };
+
+  let grid_template_rows = move || match direction {
+    SplitDirection::Row => "100%".into(),
+    SplitDirection::Column => {
+      format!("{:.2}% {:.2}%", col.get(), 100.0 - col.get())
+    }
+  };
+
+  let handle_style = move || {
+    let styles = match direction {
+      SplitDirection::Row => {
+        format!(
+          "cursor: col-resize; width: 20px; transform: translateX(-50%, 0%); \
+           top: 0; bottom: 0; left: calc({:.2}% - 10px)",
+          col.get()
+        )
+      }
+      SplitDirection::Column => {
+        format!(
+          "cursor: row-resize; height: 20px; transform: translateY(-50%, 0%); \
+           top: calc({:.2}% - 10px); left: 0; right: 0",
+          col.get()
+        )
+      }
+    };
+    format!("position: absolute; z-index: 1; {}", styles)
   };
 
   view! {
@@ -54,21 +125,14 @@ pub fn ResizableGrid(
     >
       <div
         class=move || handle_class.get()
-        style:position="absolute"
-        style:z-index="1"
-        style:width="20px"
-        style:top="0"
-        style:bottom="0"
-        style:left=move || format!("calc({:.2}% - 10px)", col.get())
-        style:transform="translateX(-50%, 0%)"
-        style:cursor="ew-resize"
+        style=handle_style
         on:mousedown=on_mouse_down
-        // on:mousemove=on_mouse_move
         on:mouseup=stop_resize
       />
       <div
         style:display="grid"
-        style:grid-template-columns=move || format!("{:.2}% {:.2}%", col.get(), 100.0 - col.get())
+        style:grid-template-columns=grid_template_columns
+        style:grid-template-rows=grid_template_rows
         style:width="100%"
         style:height="100%"
       >
